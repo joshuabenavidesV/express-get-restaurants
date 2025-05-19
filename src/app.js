13607d2b864611d7b573a9aac9b637e141c5d8b8
Joshua Benavides <joshua.benavides@verizon.com>
2025-05-19 15:08:26 -0500

    Created CRUD except no read for restaurants repo

--- src/app.js
+++ src/app.js
@@ -3,11 +3,54 @@ const app = express();
 const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
+
 //TODO: Create your GET Request Route Below:
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/restaurants",async(request,response) => {
-    const restaurants = await Restaurant.findAll({});
+    const restaurants = await Restaurant.findAll();
     response.json(restaurants);
 })
+
+app.get("/restaurants/:id", async (request, response) => {
+  const id = request.params.id;
+  const restaurant = await Restaurant.findByPk(id);
+  if (restaurant) {
+    response.json(restaurant);
+  } else {
+    response.status(404).json({ error_message: "Restaurant not found" });
+  }
+});
+// Creating a new resturant
+app.post("/restaurants", async (request, response) => {
+     const newRestaurant = await Restaurant.create(request.body);
+     response.json(newRestaurant);
+});
+//Update
+app.put("/restaurants/:id", async (request, response) => {
+  const id = request.params.id;
+  await Restaurant.update(request.body, { where: { id } });
+  const updatedRestaurant = await Restaurant.findByPk(id);
+  response.json(updatedRestaurant);
+});
+// Deleting restaurant 
+app.delete("/restaurants/:id", async (request, response) => {
+  const id = request.params.id;
+  const deleted = await Restaurant.destroy({ where: { id } });
+  if (deleted) {
+    response.json({ message: "Restaurant deleted" });
+  } else {
+    response.status(404).json({ message: "Restaurant not found" });
+  }
+});
+
+// Creating a new resturant in a specific ID  Wanted to see if I could get the ID spot 4 back
+// app.post("/restaurants/:id", async (request, response) => {
+//     const id = request.params.id;
+//      const newRestaurant = await Restaurant.create(request.body, { where: { id } });
+//      response.json(newRestaurant);
+// });
+
 
 module.exports = app;
