c5f3f0ca8b028009b3a2aa1c35c9321659a76f5e
Joshua Benavides <joshua.benavides@verizon.com>
2025-05-18 22:29:27 -0500

    wrote get for resturants

--- src/app.js
+++ src/app.js
@@ -5,4 +5,9 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below:
 
+app.get("/resturants",async(request,response) => {
+    const resturants = await Restaurant.findAll({});
+    response.json(resturants);
+})
+
 module.exports = app;
